04b6d1fa2fc26d106fdd952782b29e7b5fd2bdde
brt0 <hubert.wagner.imp@gmail.com>
2026-01-03 00:07:11 +0100

    Elfy i orki

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Net.Security;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Simulator
 {
-    public class Creature
+    public abstract class Creature
     {
         private string name = "Unknown";
         public string Name
@@ -46,6 +50,12 @@ namespace Simulator
             }
         }
 
+        public abstract int Power
+        {
+            get;
+        }
+
+
         public void Upgrade()
         {
             if (level < 10)level++;
@@ -55,7 +65,7 @@ namespace Simulator
         public string Info =>  $"{Name} [{Level}]"; 
 
 
-        public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        public abstract void SayHi();
 
 
         public static void Slogan()
@@ -90,7 +100,90 @@ namespace Simulator
             this.Level = level;
         }
 
-        public Creature() { }
+        public Creature() 
+        {
+
+        }
 
     }
+
+    public class Elf : Creature
+    {
+        private int agility;
+        public int Agility
+        {
+            get => agility;
+            init
+            {
+                if (value < 0) agility = 0;
+                else if (value > 10) agility = 10;
+                else agility = value;
+            }
+        }
+
+        private int stacks=0;
+        public void Sing()
+        {
+            Console.WriteLine($"{Name} is singing.");
+
+            stacks++;
+            if ((stacks == 3) &&(agility < 10)) { agility++; stacks = 0; }
+            
+            
+        }
+        public override void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}, my agility is {Agility}.");
+
+        public override int Power => 8 * Level + 2 * agility;
+
+        public Elf(string name, int level = 1, int agility = 0) : base(name, level)
+        {
+            Agility = agility;
+        }
+        public Elf()
+        {
+
+        }
+    }
+
+    public class Orc : Creature
+    {
+        private int rage;
+        public int Rage
+        {
+            get => rage;
+            init
+            {
+                if (value < 0) rage = 0;
+                else if (value > 10) rage = 10;
+                else rage = value;
+            }
+        }
+
+        private int stacks=0;
+        public void Hunt()
+        {
+            Console.WriteLine($"{Name} is hunting.");
+            stacks++;
+            if ((stacks == 2) && (rage < 10)){ rage++; stacks = 0; }
+        }
+
+        public override void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}, my rage is {Rage}.");
+        
+        public override int Power => 7*Level+3*rage;
+
+
+        public Orc(string name, int level = 1, int rage = 0) : base(name, level)
+        {
+            Rage = rage;
+        }
+
+        public Orc() 
+        {
+
+        }
+
+    }
+
+
+
 }
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -2,72 +2,108 @@
 {
     internal class Program
     {
-        static void TestCreatures()
+        //    static void TestCreatures()
+        //    {
+        //        Creature c = new() { Name = "   Shrek    ", Level = 20 };
+        //        c.SayHi();
+        //        c.Upgrade();
+        //        Console.WriteLine(c.Info);
+
+        //        c = new("  ", -5);
+        //        c.SayHi();
+        //        c.Upgrade();
+        //        Console.WriteLine(c.Info);
+
+        //        c = new("  donkey ") { Level = 7 };
+        //        c.SayHi();
+        //        c.Upgrade();
+        //        Console.WriteLine(c.Info);
+
+        //        c = new("Puss in Boots – a clever and brave cat.");
+        //        c.SayHi();
+        //        c.Upgrade();
+        //        Console.WriteLine(c.Info);
+
+        //        c = new("a                            troll name", 5);
+        //        c.SayHi();
+        //        c.Upgrade();
+        //        Console.WriteLine(c.Info);
+
+        //        var a = new Animals() { Description = "   Cats " };
+        //        Console.WriteLine(a.Info);
+
+        //        a = new() { Description = "Mice           are great", Size = 40 };
+        //        Console.WriteLine(a.Info);
+        //    }
+        //    static void TestDirections()
+        //    {
+        //        Creature c = new("Shrek", 7);
+        //        c.SayHi();
+
+        //        Console.WriteLine("\n* Up");
+        //        c.Go(Direction.Up);
+
+        //        Console.WriteLine("\n* Right, Left, Left, Down");
+        //        Direction[] directions = {
+        //    Direction.Right, Direction.Left, Direction.Left, Direction.Down
+        //};
+        //        c.Go(directions);
+
+        //        Console.WriteLine("\n* LRL");
+        //        c.Go("LRL");
+
+        //        Console.WriteLine("\n* xxxdR lyyLTyu");
+        //        c.Go("xxxdR lyyLTyu");
+        //    }
+        static void TestElfsAndOrcs()
         {
-            Creature c = new() { Name = "   Shrek    ", Level = 20 };
-            c.SayHi();
-            c.Upgrade();
-            Console.WriteLine(c.Info);
+            Console.WriteLine("HUNT TEST\n");
+            var o = new Orc() { Name = "Gorbag", Rage = 7 };
+            o.SayHi();
+            for (int i = 0; i < 10; i++)
+            {
+                o.Hunt();
+                o.SayHi();
+            }
 
-            c = new("  ", -5);
-            c.SayHi();
-            c.Upgrade();
-            Console.WriteLine(c.Info);
+            Console.WriteLine("\nSING TEST\n");
+            var e = new Elf("Legolas", agility: 2);
+            e.SayHi();
+            for (int i = 0; i < 10; i++)
+            {
+                e.Sing();
+                e.SayHi();
+            }
 
-            c = new("  donkey ") { Level = 7 };
-            c.SayHi();
-            c.Upgrade();
-            Console.WriteLine(c.Info);
-
-            c = new("Puss in Boots – a clever and brave cat.");
-            c.SayHi();
-            c.Upgrade();
-            Console.WriteLine(c.Info);
-
-            c = new("a                            troll name", 5);
-            c.SayHi();
-            c.Upgrade();
-            Console.WriteLine(c.Info);
-
-            var a = new Animals() { Description = "   Cats " };
-            Console.WriteLine(a.Info);
-
-            a = new() { Description = "Mice           are great", Size = 40 };
-            Console.WriteLine(a.Info);
-        }
-        static void TestDirections()
-        {
-            Creature c = new("Shrek", 7);
-            c.SayHi();
-
-            Console.WriteLine("\n* Up");
-            c.Go(Direction.Up);
-
-            Console.WriteLine("\n* Right, Left, Left, Down");
-            Direction[] directions = {
-        Direction.Right, Direction.Left, Direction.Left, Direction.Down
+            Console.WriteLine("\nPOWER TEST\n");
+            Creature[] creatures = {
+        o,
+        e,
+        new Orc("Morgash", 3, 8),
+        new Elf("Elandor", 5, 3)
     };
-            c.Go(directions);
-
-            Console.WriteLine("\n* LRL");
-            c.Go("LRL");
-
-            Console.WriteLine("\n* xxxdR lyyLTyu");
-            c.Go("xxxdR lyyLTyu");
+            foreach (Creature creature in creatures)
+            {
+                Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
+            }
         }
-
 
         static void Main(string[] args)
         {
             //HUD:
 
             Console.WriteLine("Starting Simulator!\n");
-            
+            TestElfsAndOrcs();
             //TESTS:
-            
-            Creature creature = new("ben dover",6);
 
-            TestDirections();
+
+            //Elf e = new Elf("Kerillian", 2, -1);
+            //e.SayHi();
+
+
+            //Orc o = new Orc("Gorbag", rage: 12, level: 2);
+            //o.SayHi();
+
 
 
 
